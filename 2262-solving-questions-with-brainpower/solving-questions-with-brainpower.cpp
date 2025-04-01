f06b8b0e8acf50118d5feae02534efdda5533e01
kavyachandarkar <156076079+kavyachandarkar@users.noreply.github.com>
2025-04-01 19:31:51 +0530

    Time: 0 ms (100.00%) | Memory: 119.7 MB (86.16%) - LeetSync

---
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long mostPoints(vector<vector<int>>& arr) {
+        int n = arr.size();
+        vector<long long> dp(n + 1, 0); 
+
+        for (int i = n - 1; i >= 0; i--) {
+            int next = i + arr[i][1] + 1; 
+            long long take = arr[i][0] + (next < n ? dp[next] : 0); 
+            long long skip = dp[i + 1]; 
+            dp[i] = max(take, skip); 
+        }
+
+        return dp[0]; 
+    }
+};
