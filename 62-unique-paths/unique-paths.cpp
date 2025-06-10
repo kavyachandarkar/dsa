6f77de698f691dbf67517d01b7500a06fa03b634
kavyachandarkar <156076079+kavyachandarkar@users.noreply.github.com>
2025-06-10 15:13:04 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (9.02%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    int u(int i, int j, vector<vector<int>>&dp) {
-
-        if (i == 0 && j == 0) return 1;
-        if (i < 0 || j < 0) return 0;
-
-        if (dp[i][j] != -1){
-            return dp[i][j];
+    int uniquePaths(int m, int n) {
+        // Declare the DP table with dimensions m x n
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = 1;
+                } else {
+                    int up = (i > 0) ? dp[i - 1][j] : 0;
+                    int left = (j > 0) ? dp[i][j - 1] : 0;
+                    dp[i][j] = up + left;
+                }
+            }
         }
 
-        int left = u(i, j - 1 ,dp);
-        int up = u(i - 1, j, dp);
-
-        return dp[i][j] = left + up;
-    }
-
-    int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m, vector<int>(n, -1));
-        return u(m - 1, n - 1,dp);
+        return dp[m - 1][n - 1];
     }
 };
