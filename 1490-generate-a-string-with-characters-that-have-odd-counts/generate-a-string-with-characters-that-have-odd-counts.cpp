1fef575f365b4e64067db1222fc893774118bc4f
kavyachandarkar <156076079+kavyachandarkar@users.noreply.github.com>
2025-08-20 16:07:01 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (41.28%) - LeetSync

--- 1490-generate-a-string-with-characters-that-have-odd-counts/generate-a-string-with-characters-that-have-odd-counts.cpp
+++ 1490-generate-a-string-with-characters-that-have-odd-counts/generate-a-string-with-characters-that-have-odd-counts.cpp
@@ -1,21 +1,12 @@
 class Solution {
 public:
     std::string generateTheString(int n) {
-        std::string ans = "";
         
         if (n % 2 == 0) {
-           
-            ans.push_back('a'); 
-            for (int i = 0; i < n - 1; ++i) {
-                ans.push_back('b'); 
-            }
+            return std::string(n - 1, 'a') + 'b';
         } else {
            
-            for (int i = 0; i < n; ++i) {
-                ans.push_back('a');
-            }
+            return std::string(n, 'a');
         }
-        
-        return ans;
     }
 };
